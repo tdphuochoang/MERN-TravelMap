4af640800bf0b195fed62d93b006866348755672
Hoang <tdll.trdphoang@gmail.com>
2022-08-05 11:13:55 -0700

    Done backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const pinRoute = require("./routes/pins.js");
+const userRoute = require("./routes/users.js");
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => {
+		console.log("MongoDB connected");
+	})
+	.catch((err) => console.log(err));
+
+//Middleware
+app.use(express.json());
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
+
+app.listen(8800, () => {
+	console.log("Backend server is running");
+});
